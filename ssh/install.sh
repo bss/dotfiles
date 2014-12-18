261c5ad0e0a0098fc1503362985341c7d6adc5c0
bss <github@eflow.dk>
2014-12-18 14:49:33 +0700

    Add ssh_config

---
+++ ssh/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# SSH Config
+#
+# This installs a nice SSH config
+
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd)
+SSH_DIR="$HOME/.ssh"
+
+set -e
+
+echo ''
+
+echo 'Creating tmp dir for ssh'
+mkdir "$SSH_DIR/tmp"
+
+echo 'Linking ssh config'
+ln -s "$DOTFILES_ROOT/ssh/ssh_config" "$SSH_DIR/config"
+
+exit 0
+
