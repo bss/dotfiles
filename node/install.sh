daf6ef4a0316b12348b1c4f3edc90507bbe2489a
Bo Stendal Sorensen <bo@stendal-sorensen.net>
2017-10-16 12:28:40 -0700

    Also install yarn

--- node/install.sh
+++ node/install.sh
@@ -5,3 +5,9 @@ then
   echo "  Installing node for you."
   brew install node > /tmp/node-install.log
 fi
+
+if test ! $(which yarn)
+then
+  echo "  Installing yarn you."
+  brew install yarn > /tmp/node-install.log
+fi
